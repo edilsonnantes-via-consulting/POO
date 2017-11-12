519e032138e69287f0b03e3008f45ac7ecba3244
Gustavo de Almeida Duarte <31395476+guga7297@users.noreply.github.com>
2017-11-12 16:48:26 -0200

    Aprendendo a usar o git

---
+++ Utilitario.java
@@ -0,0 +1,13 @@
+public class Utilitario{
+	//Custos predefinidos para o uso
+	public float custoBasico = 100.0;
+	public float custoDependente = 40.0;
+	
+	//Estaticos para auxiliar o controle de IDs em todas as classes
+	public static int ENDERECO_ID = 1;
+	public static int MODALIDADE_ID = 1;
+	public static int SOCIO_ID = 1;
+	public static int TITULO_ID = 1;
+	public static int MENSALIDADE_ID = 1;
+	public static int MATRICULA_ID = 1;
+}
