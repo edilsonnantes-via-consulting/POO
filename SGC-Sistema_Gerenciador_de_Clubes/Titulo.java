07181622224ee32b12b3ee63d8a8de7f488bb878
yuggyohh08 <33269041+yuggyohh08@users.noreply.github.com>
2017-11-23 22:04:26 -0200

    Add files via upload

---
+++ SGC-Sistema_Gerenciador_de_Clubes/Titulo.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)Titulo.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/11/23
+ */
+
+
+public class Titulo {
+
+    private int tituloid;
+    private Socio titular;
+    private Mensalidade mensalidades[];
+    
+    
+    public void getTituloid(){
+    	return this.tituloid;
+    }
+    
+    public void getTitular(){
+    	return this.titular;
+    }
+    
+    public void getMensalidades(){
+    	return this.mensalidades;
+    }
+    
+    public Titulo criar(Endereco enderecos[]){
+    	
+    }
+    
+    public Titulo pesquisar(int id, Titulo titulos[]){
+    	for(int i = 0; i <= titulos.lentgh; i++){
+    		if(id == titulos[i].getTituloid()){
+    			return titulos[i];
+    		}
+    		else{
+    			return NULL;
+    		}
+    	}
+    }
+}
