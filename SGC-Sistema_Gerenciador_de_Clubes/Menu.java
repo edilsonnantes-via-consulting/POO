c5f4e7816a1f1be5187359bb54ef81bf34a60925
Gustavo de Almeida Duarte <31395476+guga7297@users.noreply.github.com>
2017-12-03 01:12:56 -0200

    Foi implementado métodos de cadastro

--- SGC-Sistema_Gerenciador_de_Clubes/Menu.java
+++ SGC-Sistema_Gerenciador_de_Clubes/Menu.java
@@ -4,6 +4,12 @@ public class Menu
     {
         while(true)
         {
+            Modalidade mod[] = new Modalidade[Utilitario.TOTAL_MODALIDADES];
+            Titulo usuario[] = new Titulo[Utilitario.TOTAL_SOCIOS];
+            Socio soc[] = new Socio[Utilitario.TOTAL_TITULOS];
+            Endereco end[] = new Endereco[Utilitario.TOTAL_ENDERECOS];
+            int opcao = 0; //Variavel que armazena a opção escolhida pelo usuário
+    
             System.out.printf("\n\n\t\t _____________ Menu Principal ____________\n");
             System.out.printf("\t\t|\t                                  |\n");
             System.out.printf("\t\t|\t<1> Cadastrar Modalidade          |\n");
@@ -11,96 +17,87 @@ public class Menu
             System.out.printf("\t\t|\t<3> Matricular Socio              |\n");
             System.out.printf("\t\t|\t<4> Desmatricular Socio           |\n");
             System.out.printf("\t\t|\t<5> Pagar Mensalidade             |\n");
-    //        System.out.printf("                                | <5> |\n");
             System.out.printf("\t\t|\t<6> Sair                          |\n");
             System.out.printf("\t\t|_________________________________________|\n\n");
 
-            System.out.printf("Qual a opcao escolhida??\n");
+            opcao = Leitura.lerInt("Qual a opcao escolhida??\n");
 
-            cases(Leitura.lerInt());
-
-            for (int i = 0; i < 50; ++i)
-                System.out.printf("\n");
+            switch(opcao)
+            {
+                case 1:
+                    cadastrarModalidade(mod);
+                    break;
+    
+                case 2:
+                    cadastrarTitulo(usuario, end);
+                    break;
+    
+                case 3:
+                    System.out.printf("Qual ID voce quer matricular??\n");
+                    int auxId = Leitura.lerInt();
+    
+                    System.out.printf("Qual a ID da modalidade??\n");
+                    int auxMod = Leitura.lerInt();
+    
+                    if(Socio.pesquisar(auxId, soc) == null || Modalidade.pesquisar(auxMod, mod) == null)
+                        soc[auxId].matricular(mod[auxMod].getId(), mod);
+    
+                    else
+                        System.out.printf("\nInvalido..\n");
+    
+                    break;
+    
+                case 4:
+                    Socio soc_pesquisa = Socio.pesquisar(Leitura.lerInt("Qual ID do socio: "), soc);
+                    if(soc_pesquisa == null)
+                        System.out.printf("ID invalida..\n");
+                    else
+                        soc_pesquisa.desmatricular(Leitura.lerInt("Qual ID voce quer desmatricular?? \n"));
+                    
+                    break;
+    
+                case 5:
+                    if(Utilitario.TITULO_ID != 1)
+                    {
+                        Titulo tit_pesquisa = Titulo.pesquisar(Leitura.lerInt("Qual ID do socio: "), usuario);
+                        if(tit_pesquisa == null)
+                            System.out.printf("ID invalida..\n");
+                        else
+                        {
+                            if(tit_pesquisa.pagarMensalidade(Leitura.lerInt("Digite o mes: ")))
+                                System.out.printf("Pagamento realizado com sucesso..");
+    
+                            else
+                                System.out.printf("Erro no Pagamento..");
+                        }
+                    }
+    
+                    else
+                        System.out.printf("Impossivel pagar..");
+    
+                    break;
+    
+                default:
+                    System.exit(0);
+            }
+         }
+    }
+   
+    public static void cadastrarModalidade(Modalidade mod[]){
+        if(Utilitario.MODALIDADE_ID < Utilitario.TOTAL_MODALIDADES){
+            mod[Utilitario.MODALIDADE_ID - 1] = Modalidade.criar();
+            System.out.printf("Cadastro efetuado com sucesso!!");
+        }else{
+            System.out.printf("\nNumero maximo de modalidades cadastradas..\n"); 
         }
     }
 
-    public static void cases(int opc)
-    {
-        Modalidade mod[] = new Modalidade[Utilitario.TOTAL_MODALIDADES];
-        Titulo usuario[] = new Titulo[Utilitario.TOTAL_SOCIOS];
-        Socio soc[] = new Socio[Utilitario.TOTAL_TITULOS];
-        Endereco end[] = new Endereco[Utilitario.TOTAL_ENDERECOS];
-
-        switch(opc)
-        {
-            case 1:
-                if(Utilitario.MODALIDADE_ID < Utilitario.TOTAL_MODALIDADES)
-                    mod[Utilitario.MODALIDADE_ID - 1] = Modalidade.criar();
-
-                else
-                    System.out.printf("\nNumero maximo de modalidades cadastradas..\n");
-
-                break;
-
-            case 2:
-                if(Utilitario.TITULO_ID < Utilitario.TOTAL_TITULOS)
-                {
-                    end[Utilitario.ENDERECO_ID - 1] = Endereco.criar();
-                    usuario[Utilitario.TITULO_ID - 1] = Titulo.criar(end);
-                }
-
-                else
-                    System.out.printf("\nNumero maximo de socios cadastrados..\n");
-
-                break;
-
-            case 3:
-                System.out.printf("Qual ID voce quer matricular??\n");
-                int auxId = Leitura.lerInt();
-
-                System.out.printf("Qual a ID da modalidade??\n");
-                int auxMod = Leitura.lerInt();
-
-                if(Socio.pesquisar(auxId, soc) == null || Modalidade.pesquisar(auxMod, mod) == null)
-                    soc[auxId].matricular(mod[auxMod].getId(), mod);
-
-                else
-                    System.out.printf("\nInvalido..\n");
-
-                break;
-
-            case 4:
-                Socio soc_pesquisa = Socio.pesquisar(Leitura.lerInt("Qual ID do socio: "), soc);
-                if(soc_pesquisa == null)
-                    System.out.printf("ID invalida..\n");
-                else
-                    soc_pesquisa.desmatricular(Leitura.lerInt("Qual ID voce quer desmatricular?? \n"));
-                
-                break;
-
-            case 5:
-                if(Utilitario.TITULO_ID != 1)
-                {
-                    Titulo tit_pesquisa = Titulo.pesquisar(Leitura.lerInt("Qual ID do socio: "), usuario);
-                    if(tit_pesquisa == null)
-                        System.out.printf("ID invalida..\n");
-                    else
-                    {
-                        if(tit_pesquisa.pagarMensalidade(Leitura.lerInt("Digite o mes: ")))
-                            System.out.printf("Pagamento realizado com sucesso..");
-
-                        else
-                            System.out.printf("Erro no Pagamento..");
-                    }
-                }
-
-                else
-                    System.out.printf("Impossivel pagar..");
-
-                break;
-
-            default:
-                System.exit(0);
+    public static void cadastrarTitulo(Titulo usuario[], Endereco end[]){
+        if(Utilitario.TITULO_ID < Utilitario.TOTAL_TITULOS){
+            usuario[Utilitario.TITULO_ID - 1] = Titulo.criar(end);
+            System.out.printf("Cadastro efetuado com sucesso!!");
+        }else{
+            System.out.printf("\nNumero maximo de socios cadastrados..\n");
         }
     }
 }
